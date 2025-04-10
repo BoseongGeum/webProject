e61da804356196ad1f32c431da139d54bb564140
BoseongGeum <rmaqhtjd8364@naver.com>
2025-04-10 09:51:15 +0900

    fix: Navbar 모바일 환경 개선

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,8 +13,9 @@ export default function Navbar() {
             }`}
         >
             {/* 네비게이션 바 */}
-            <div className="bg-black bg-opacity-70 w-64 px-4 py-3 flex items-center justify-center rounded-b-full shadow-lg
-                sm:w-44 sm:px-3 sm:py-2">
+            <div className="bg-black bg-opacity-70 text-white w-44 px-3 py-2 flex items-center justify-center rounded-b-full shadow-lg
+                sm:w-64 sm:px-4 sm:py-3"
+            >
                 <Link
                     to="/"
                     onClick={() => setIsOpen(true)}
@@ -23,7 +24,7 @@ export default function Navbar() {
                     <img
                         src={logo}
                         alt="CBOL Logo"
-                        className="h-10 w-auto sm:h-8"
+                        className="h-8 w-auto sm:h-10"
                     />
                 </Link>
             </div>
@@ -31,14 +32,14 @@ export default function Navbar() {
             {/* 토글 버튼 */}
             <button
                 onClick={() => setIsOpen(!isOpen)}
-                className="absolute left-1/2 top-full mt-2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-70 text-white p-1 w-6 h-6 flex items-center justify-center rounded-b-full border border-black hover:border-white shadow transition-all
-                sm:p-0.5 sm:w-5 sm:h-5"
+                className="absolute left-1/2 top-full mt-2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-70 text-white p-0.5 w-5 h-5 flex items-center justify-center rounded-b-full border border-black hover:border-white shadow transition-all
+                sm:p-1 sm:w-6 sm:h-6"
                 aria-label="Toggle Navbar"
             >
                 {isOpen ? (
-                    <ChevronUp size={14} className="sm:w-3 sm:h-3" />
+                    <ChevronUp size={14} className="w-3 h-3 sm:w-4 sm:h-4" />
                 ) : (
-                    <ChevronDown size={14} className="sm:w-3 sm:h-3" />
+                    <ChevronDown size={14} className="w-3 h-3 sm:w-4 sm:h-4" />
                 )}
             </button>
         </div>
