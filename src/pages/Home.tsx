b998748456fef8f031ff95ef30008cdf9ac4a7b9
BoseongGeum <rmaqhtjd8364@naver.com>
2025-05-19 10:35:13 +0900

    fix: 반응형 개선

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -102,7 +102,7 @@ export default function Home() {
                             }}
                             initial={{ opacity: 0, y: 20 }}
                             animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 1, delay: 2.0 }}
+                            transition={{ duration: 1 }}
                         >
                             <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-75 transition-all duration-300" />
                             <div className="absolute inset-0 flex items-center justify-center text-white text-[clamp(14px,1.2vw,20px)] opacity-0 group-hover:opacity-100 transition-all duration-300">
@@ -133,7 +133,7 @@ export default function Home() {
                             }}
                             initial={{ opacity: 0, y: 20 }}
                             animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 1, delay: 2.3 }}
+                            transition={{ duration: 1 }}
                         >
                             <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-75 transition-all duration-300" />
                             <div className="absolute inset-0 flex items-center justify-center text-white text-[clamp(14px,1.2vw,20px)] opacity-0 group-hover:opacity-100 transition-all duration-300">
