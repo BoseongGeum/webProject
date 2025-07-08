bcbe91781579fa5604f31f9bf50f3ef2620dbd76
BoseongGeum <rmaqhtjd8364@naver.com>
2025-07-08 09:39:16 +0900

    fix: 팀1 탭 임시 추가

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ const menus = [
     { name: "회사소개", path: "/team2/koreaOffice" },
     { name: "서비스", path: "/team2/ourServices" },
     { name: "Contact", path: "/team2/contactUs" },
+    { name: "Team1(임시)", path: "/team1" },
 ];
 
 function AppContent() {
